dd4acef73f653a7539eaf17d0ca09a52ef39f721
Tarciso Filho <tarciso.filho@hotmail.com>
2017-05-17 16:51:01 -0300

    Inicia

--- src/VO/LivroVO.java
+++ src/VO/LivroVO.java
@@ -1,33 +1,59 @@
 package VO;
 
-public class LivroVO extends ProdutoVO{
+public class LivroVO extends ProdutoVO
+{
 
-	private String autor = null;
-	private String editora = null;
-	private int anoDeEdicao;
-	
-	public String getAutor(){
-		return this.autor;
-	}
-	
-	public void setAutor(String autor){
-		this.autor = autor;
-	}
-	
-	public void setEditora(String editora){
-		this.editora = editora;
-	}
-	
-	public String getEditora(){
-		return this.editora;
-	}
-	
-	public void setAnoDeEdicao(int anoDeEdicao){
-		this.anoDeEdicao = anoDeEdicao;
-	}
-	
-	public int getAnoDeEdicao(){
-		return this.anoDeEdicao;
-	}
-	
+    private String autor = null;
+    private String editora = null;
+    private int anoDeEdicao;
+
+    public String getAutor(){
+            return this.autor;
+    }
+
+    public void setAutor(String autor){
+            this.autor = autor;
+    }
+
+    public void setEditora(String editora){
+            this.editora = editora;
+    }
+
+    public String getEditora(){
+            return this.editora;
+    }
+
+    public void setAnoDeEdicao(int anoDeEdicao){
+            this.anoDeEdicao = anoDeEdicao;
+    }
+
+    public int getAnoDeEdicao(){
+            return this.anoDeEdicao;
+    }
+    
+    private String escreverlinha1()
+    {
+        return "Livro " + this.getCodigoDoProduto() + "/n";
+    }
+    
+    private String escreverlinha2()
+    {
+        return this.getAutor() + ". (" + this.getAnoDeEdicao() + "). ";
+    }
+    
+    private String escreverlinha3()
+    {
+        return this.getTitulo()+ ". " + this.getEditora()+ "./n";
+    }
+    
+    private String escreverReferencia()
+    {
+        return (this.escreverlinha1() + this.escreverlinha2() + this.escreverlinha3());
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.escreverReferencia();
+    }
 }
